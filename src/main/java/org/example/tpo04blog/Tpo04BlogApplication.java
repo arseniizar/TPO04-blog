1686978e90f129034e508b1b1cbb17454eb4d21b
arseniizar <arseniizarudniuk@gmail.com>
2025-03-29 20:42:47 +0100

    initial commit

---
+++ src/main/java/org/example/tpo04blog/Tpo04BlogApplication.java
@@ -0,0 +1,13 @@
+package org.example.tpo04blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpo04BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tpo04BlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tpo04blog/Tpo04BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tpo04blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpo04BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
